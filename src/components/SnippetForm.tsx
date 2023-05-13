6620b67e8adc1abc20cddc168c70093d7f7ec97c
Diego Rubí Salas <diegoalru@gmail.com>
2023-05-12 22:59:09 -0600

    Agregado la muestra de mensajes de error al crear un archivo.

--- src/components/SnippetForm.tsx
+++ src/components/SnippetForm.tsx
@@ -1,34 +1,61 @@
-import React, { useState } from 'react'
-import { writeFile } from '@tauri-apps/api/fs'
+import React, { useState } from "react";
+import { writeFile, exists } from "@tauri-apps/api/fs";
 import { documentDir } from "@tauri-apps/api/path";
+import { toast } from "react-hot-toast";
 
 function SnippetForm() {
-
-  const [snippetName, setSnippetName] = useState<string>()
+  const [snippetName, setSnippetName] = useState<string>();
 
   async function submitHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    await createSnippet();
-  }
-  
-  async function createSnippet() {
 
-if (!snippetName) {
-      alert('Please enter a snippet name');
+    toast.promise(
+      createSnippet(),
+      {
+        loading: "Creating snippet...",
+        success: "Snippet created!",
+        error: "Error creating snippet",
+      },
+      {
+        duration: 3000,
+        position: "bottom-right",
+        style: {
+          background: "#333",
+          color: "#fff",
+        },
+      }
+    );
+  }
+
+  async function createSnippet() {
+    if (!snippetName) {
+      alert("Please enter a snippet name");
       return;
     }
 
     // Verificar que el nombre no exceda los 255 bytes (255 caracteres).
     if (snippetName!.length > 255) {
-      alert('Snippet name is too long');
+      alert("Snippet name is too long");
       return;
     }
 
+    if (await existsFile()) {
+      throw new Error("Snippet already exists");
+    }
 
-    let path = await documentDir();
-    writeFile(`${path}/tauriDocs/${snippetName}.json`, '{}');
+    await createFile();
   }
 
+  async function createFile() {
+    let path = await documentDir();
+    await writeFile(`${path}/tauriDocs/${snippetName}`, "");
+  }
+
+  async function existsFile() {
+    let path = await documentDir();
+    let response = await exists(`${path}/tauriDocs/${snippetName}`);
+    return response;
+  }
 
   return (
     <form onSubmit={(e) => submitHandler(e)}>
@@ -38,9 +65,12 @@ if (!snippetName) {
         placeholder="Snippet Name"
         onChange={(e) => setSnippetName(e.target.value)}
       />
-      <button className="hidden" type="submit" />
+      <button
+        className="hidden"
+        type="submit"
+      />
     </form>
   );
 }
 
-export default SnippetForm
+export default SnippetForm;
