c135f5c999274d0ce3dc84e921b64a84cf3b56d8
Diego Rubí Salas <diegoalru@gmail.com>
2023-05-27 02:08:38 -0600

    Cambio de formato y nombre de variables.

--- src/store/SnippetStore.ts
+++ src/store/SnippetStore.ts
@@ -1,33 +1,28 @@
 import { create } from "zustand";
+import {devtools} from "zustand/middleware";
 
-interface Snippet {
-  name: string;
-  code: string | null;
+interface Snippet{
+    name: string;
+    code: string | null;
 }
 
 interface SnippetState {
-  snippetsName: string[];
+  snippetName: string[];
   selectedSnippet: Snippet | null;
-  addSnippet: (snippetName: string) => void;
-  setSnippetsName: (snippetsName: string[]) => void;
+  setSnippetName: (snippetName: string[]) => void;
   setSelectedSnippet: (snippetName: Snippet | null) => void;
+  addSnippet: (snippetName: string) => void;
   removeSnippet: (snippetName: string) => void;
 }
 
-export const useSnippetStore = create<SnippetState>((set) => ({
-  snippetsName: [],
-  selectedSnippet: null,
-  addSnippet: (snippetName: string) =>
-    set((state) => ({
-      snippetsName: [...state.snippetsName, snippetName],
-    })),
-  setSnippetsName: (snippetsName: string[]) => set({ snippetsName }),
-  setSelectedSnippet: (selectedSnippet: Snippet | null) =>
-    set({ selectedSnippet }),
-  removeSnippet: (snippetName: string) =>
-    set((state) => ({
-      snippetsName: state.snippetsName.filter(
-        (item) => item !== snippetName
-      ),
-    })),
-}));
+export const useSnippetStore = create<SnippetState>()(
+    devtools((set) => ({
+        snippetName: [],
+        selectedSnippet: null,
+        setSnippetName: (snippetName) => set({ snippetName }),
+        setSelectedSnippet: (snippetName) => set({ selectedSnippet: snippetName }),
+        addSnippet: (snippetName) => set((state) => ({ snippetName: [...state.snippetName, snippetName] })),
+        removeSnippet: (snippetName) => set((state) => ({ snippetName: state.snippetName.filter((name) => name !== snippetName) })),
+    }))
+);
+
